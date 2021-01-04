6e6609c69785990ab22e378e4ad27def6749799d
StefanoCalandra <76255813+StefanoCalandra@users.noreply.github.com>
2021-01-04 15:31:32 +0100

    Update generator.h

--- generator.h
+++ generator.h
@@ -1,5 +1,10 @@
-#ifndef __GENERATOR_H_
 #define __GENERATOR_H_
 #include "general.h"
+
+void logmsg(char *message) {
+  int pid;
+  pid = getpid();
+  printf("[generator-%d] %s\n", pid, message);
+}
 
 #endif /* __GENERATOR_H_ */
