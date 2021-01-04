23209426abbb2241d54440e9670542d0b2185aed
StefanoCalandra <76255813+StefanoCalandra@users.noreply.github.com>
2021-01-04 15:28:44 +0100

    Update general.h

--- general.h
+++ general.h
@@ -1,36 +1,20 @@
-#ifndef __GENERAL_H_
-#define __GENERAL_H_
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/types.h>
+#include <sys/wait.h>
 #include <time.h> /* rand */
 #include <unistd.h>
 #define SO_WIDTH 4 /* a tempo di compilazione */
 #define SO_HEIGHT 4
+#define SO_WIDTH 8 /* a tempo di compilazione */
+#define SO_HEIGHT 8
+#define MAX_SOURCES (SO_WIDTH * SO_HEIGHT / 3)
 #define EXIT_ON_ERROR                                                          \
   if (errno) {                                                                 \
-    fprintf(stderr, "%d: pid %ld; errno: %d (%s)\n", __LINE__, (long)getpid(), \
-            errno, strerror(errno));                                           \
-    exit(EXIT_FAILURE);                                                        \
-  }
-
-enum type { FREE, SOURCE, HOLE };
+@ -43,33 +43,7 @@ typedef struct {
 
 typedef struct {
-  enum type state;
-  int capacity;
-  int traffic;
-  int visits;
-} Cell;
-
-typedef struct {
-  int SO_TAXI, SO_SOURCES, SO_HOLES, SO_CAP_MIN, SO_CAP_MAX, SO_TIMENSEC_MIN,
-      SO_TIMENSEC_MAX, SO_TIMEOUT, SO_DURATION;
-} Config;
+  long type;
+  Point source;
+  Point destination;
+} Message;
 
 /*
  * Print on stdout the map in a readable format:
