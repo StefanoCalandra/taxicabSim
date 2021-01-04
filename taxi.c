e990b6bef32d8ef36419f07622d440d7030bf498
StefanoCalandra <76255813+StefanoCalandra@users.noreply.github.com>
2021-01-04 15:33:56 +0100

    Update taxi.c

--- taxi.c
+++ taxi.c
@@ -1,17 +1,85 @@
 #include "taxi.h"
+#include "general.h"
+#include <stdio.h>
+#include <unistd.h>
+
+void *mapptr;
+Point position;
+int *executing;
+
+void SIGINThandler(int sig) { *executing = 0; }
+void SIGINThandler(int sig) {
+  *executing = 0;
+  logmsg("Finishing up");
+  shmdt(mapptr);
+}
+
+void incTrafficAt(Point p) {
+
+  /*wait mutex*/
+  ((Cell(*)[SO_WIDTH][SO_HEIGHT])mapptr)[p.x][p.y]->traffic++;
+  logmsg("Incrementato traffico in ");
+  printf("\t(%d,%d)\n", p.x, p.y);
+  /*signal mutex*/
+}
+
+void moveTo(Point p) { /*pathfinding*/
+  /* funzione nanosleep permette di aspettare per il tempo indicato da */
+  usleep(5000000);
+  logmsg("Moving to");
+  printf("\t(%d,%d)\n", p.x, p.y);
+  incTrafficAt(p);
+}
 
 int main(int argc, char **argv) {
-  int shmid;
-  Cell map[SO_WIDTH][SO_HEIGHT];
-  shmid = shmget(IPC_PRIVATE, SO_WIDTH * SO_HEIGHT * sizeof(Cell), 0666);
+@ -22,11 +34,15 @@ int main(int argc, char **argv) {
+  key_t shmkey, qkey;
+  Message msg;
+
+  logmsg("Inizializzazione");
+  sleep(1);
+
+  /************INIT************/
+  if ((shmkey = ftok("makefile", 'a')) < 0) {
+    EXIT_ON_ERROR
+  }
+
+  if ((shmid = shmget(shmkey, sizeof(int), IPC_CREAT | 0644)) < 0) {
+  if ((shmid = shmget(shmkey, sizeof(int), 0644)) < 0) {
+    EXIT_ON_ERROR
+  }
+
+@ -39,7 +55,9 @@ int main(int argc, char **argv) {
+    printf("ftok error\n");
+    EXIT_ON_ERROR
+  }
+  shmid = shmget(shmkey, 0, 0644);
+  if ((shmid = shmget(shmkey, 0, 0644)) < 0) {
+    EXIT_ON_ERROR
+  }
   if (shmid < 0) {
     printf("shmget error\n");
     EXIT_ON_ERROR
+@ -57,18 +75,16 @@ int main(int argc, char **argv) {
+  logmsg("Init position");
+  sscanf(argv[1], "%d", &position.x);
+  sscanf(argv[2], "%d", &position.y);
+  /************END-INIT************/
+
+  incTrafficAt(position);
+  sleep(3);
+
+  while (*executing) {
+    msgrcv(qid, &msg, sizeof(Point), 0, 0);
+    logmsg("Going to Source");
+    moveTo(msg.source);
+    logmsg("Going to Nearest Source");
+    /* moveTo(getNearSource()); */
+    logmsg("Going to destination");
+    moveTo(msg.destination);
   }
-  shmat(shmid, &map, 0);
-  printMap(&map);
 
   logmsg("Finishing up");
-  shmdt(&map);
+  shmdt(mapptr);
   exit(0);
 }
