b24a4ceba0cf0adebedbd75799e5de8cfb81a424
StefanoCalandra <76255813+StefanoCalandra@users.noreply.github.com>
2021-01-10 11:09:06 +0100

    Update generator.h

--- generator.h
+++ generator.h
@@ -2,4 +2,17 @@
 #define __GENERATOR_H_
 #include "general.h"
 
+void parseConf(Config *);
+
+int checkNoAdiacentHoles(Cell (*)[SO_WIDTH][SO_HEIGHT], int, int);
+
+void generateMap(Cell (*)[SO_WIDTH][SO_HEIGHT], Config *);
+
+void printMap(Cell (*)[SO_WIDTH][SO_HEIGHT]);
+
+void logmsg(char *, enum Level);
+
+void SIGINThandler(int);
+
+void ALARMhandler(int);
 #endif /* __GENERATOR_H_ */
