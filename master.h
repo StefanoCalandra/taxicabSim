e6df87f87a7dab3e43baadaf1a91b1e7def5609e
StefanoCalandra <76255813+StefanoCalandra@users.noreply.github.com>
2021-01-14 17:41:43 +0100

    Update master.h

--- master.h
+++ master.h
@@ -1,47 +1,16 @@
 #ifndef __MASTER_H_
 #define __MASTER_H_
-#include <errno.h>
-#include <math.h>
-#include <signal.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
-#include <sys/shm.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <time.h> /* rand */
-#include <unistd.h>
+#include "general.h"
 
-#ifndef DEBUG    /* DEBUG by setting when compiling -DDEBUG=1 */
-#define DEBUG 0
-#endif
+void ();
 
-#define SO_WIDTH 8 /* a tempo di compilazione */
-#define SO_HEIGHT 8
-#define MAX_SOURCES (SO_WIDTH * SO_HEIGHT / 3)
-#define EXIT_ON_ERROR                                                          \
-  if (errno) {                                                                 \
-    fprintf(stderr, "%d: pid %ld; errno: %d (%s)\n", __LINE__, (long)getpid(), \
-            errno, strerror(errno));                                           \
-    exit(EXIT_FAILURE);                                                        \
-  }
+void ();
 
-enum type { FREE, SOURCE, HOLE };
+void ();
 
-enum Level { RUNTIME, DB };
+void SIGINThandler(int);
 
-typedef struct {
-  int ;
-} Viaggi;
-
-typedef struct {
-  int SO_SOURCES, SO_TOP_CELLS;
-} Mappa;
-
-typedef struct {
-  int SO_TAXI, SO_TIMENSEC_MIN, SO_TIMENSEC_MAX, SO_TIMEOUT, SO_DURATION;
-} Taxi;
+void logmsg(char *, enum Level);
 
 #endif /* __MASTER_H_ */
