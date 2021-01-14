c043db675e91bd0290a33a05dc5ff1c88fd94b12
StefanoCalandra <76255813+StefanoCalandra@users.noreply.github.com>
2021-01-14 17:46:55 +0100

    Update master.c

--- master.c
+++ master.c
@@ -1,3 +1,24 @@
 #include "master.h"
 
-int main() { exit(0); }
+void ;
+int ;
+
+int main(int argc, char **argv) 
+{
+  int shmid;
+  key_t shmkey, qkey;
+  Message msg;
+}
+
+void logmsg(char *message, enum Level l) {
+  if (l <= DEBUG) {
+    printf("[taxi-%d] %s\n", getpid(), message);
+  }
+}
+
+void SIGINThandler(int sig) {
+  logmsg("Finishing up", DB);
+  shmdt(mapptr);
+  logmsg("Graceful exit successful", DB);
+  exit(0);
+}
