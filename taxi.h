ff4f97c6888faffa94d07cf10d7ccb4a2000529b
StefanoCalandra <76255813+StefanoCalandra@users.noreply.github.com>
2021-01-15 09:08:52 +0100

    Update taxi.h

--- taxi.h
+++ taxi.h
@@ -1,6 +1,7 @@
 #ifndef __TAXI_H_
 #define __TAXI_H_
 #include "general.h"
+#include <limits.h>
 
 void SIGINThandler(int);
 
@@ -9,5 +10,7 @@ void logmsg(char *, enum Level);
 void incTrafficAt(Point);
 
 void moveTo(Point);
+
+Point getNearSource();
 
 #endif /*__TAXI_H_*/
