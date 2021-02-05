2df586cb16191511ec1dcca55ab7e8a8cc5518d4
StefanoCalandra <76255813+StefanoCalandra@users.noreply.github.com>
2021-02-05 19:01:30 +0100

    Update taxi.h

--- taxi.h
+++ taxi.h
@@ -1,12 +1,37 @@
 #ifndef __TAXI_H_
 #define __TAXI_H_
 #include "general.h"
+#include <limits.h>
+#include <math.h>
 
-void logmsg(char *message) {
-  int pid;
-  pid = getpid();
-  printf("[taxi-%d] %s\n", pid, message);
-}
+void handler(int);
+
+void logmsg(char *, enum Level);
+
+void incTrafficAt(Point);
+
+void decTrafficAt(Point);
+
+void moveTo(Point);
+
+Point getNearSource();
+
+void checkTimeout();
+
+void printRep();
+
+typedef struct {
+  int distance;
+  int maxDistanceInTrip;
+  struct timeval maxTimeInTrip;
+  int clients;
+  int tripsSuccess;
+  int abort;
+} taxiData;
+
+typedef struct {
+  long type;
+  taxiData data;
+} dataMessage;
 
 #endif /*__TAXI_H_*/
- 
